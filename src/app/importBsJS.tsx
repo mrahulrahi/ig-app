37015c44e538cbac192cbd9b2d6e889086935d32
mrahulrahi <mrahulrahi@gmail.com>
2024-12-06 02:05:02 +0530

    production error fix

--- src/app/importBsJS.tsx
+++ src/app/importBsJS.tsx
@@ -1,10 +1,9 @@
 'use client'
 import { useEffect } from "react";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function ImportBsJS() {
   useEffect(() => {
-    // Bootstrap JS is now imported at the top
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
   return null;
 }
