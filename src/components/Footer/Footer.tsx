b110a109422202edd11c2cab1ddb9e217ccc4056
mrahulrahi <mrahulrahi@gmail.com>
2024-12-09 22:38:48 +0530

    footer changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,42 +3,39 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <footer id="footer">
-      <div className="container-fluid"> 
+      <div className="container-fluid">
         <div className="row">
           <div className="col-12">
-            <div className="footer-inside">
-              <div className="footer-inside-content d-flex justify-content-between">
-                <div className="footer-inside-content-item">
-                  <div className="footer-inside-content-item-logo d-flex align-items-center">
+            <div className="footer-top d-flex flex-wrap justify-content-between">
+              <div className="footer-inside">
+                <div className="footer-logo-area d-flex align-items-center justify-content-center justify-content-lg-start mb-3">
+                  <div className="footer-logo">
                     <Image src="/images/logo.png" alt="Imagine Group" width={100} height={100} />
-                    <span className="footer-inside-content-item-logo-name">Imagine <br /> Group</span>
                   </div>
-                  <div className="footer-inside-content-item-text">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-                  </div>
-                </div>  
-                <div className="footer-inside-content-item">
-                  <div className="footer-inside-content-item-title">
-                    <h3>Quick Links</h3>
-                    <ul className="list-unstyled">
-                      <li><a href="#">About Us</a></li>
-                      <li><a href="#">Features</a></li>
-                    </ul>
-                  </div>
+                  <span className="footer-name">Imagine <br /> Group</span>
                 </div>
-                <div className="footer-inside-content-item">
-                  <div className="footer-inside-content-item-title">
-                    <h3>Contact Us</h3>
-                    <ul className="list-unstyled">
-                      <li><a href="#">+91 9876543210</a></li>
-                      <li><a href="#">info@imaginergroup.com</a></li>
-                    </ul>
-                  </div>
-                </div>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
               </div>
-              <div className="footer-inside-bottom">
-                <p>&copy; {new Date().getFullYear()} Imagine Group. All rights reserved.</p>
+
+
+              <div className="footer-nav">
+                <h3>Quick Links</h3>
+                <ul className="footer-nav-menu">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Features</a></li>
+                </ul>
               </div>
+              <div className="footer-contact-info">
+                <h3>Contact Us</h3>
+                <ul className="footer-contact-list">
+                  <li><a href="#">+91 9876543210</a></li>
+                  <li><a href="#">info@imaginergroup.com</a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="footer-bottom">
+              <p>&copy; {new Date().getFullYear()} Imagine Group. All rights reserved.</p>
             </div>
           </div>
         </div>
